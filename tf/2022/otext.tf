6a4bf8b316d1fd8eeb67f80ea89740be53e53aee
Oliver Glanz <o.glanz@gmail.com>
2022-01-06 16:54:46 -0500

    Add files via upload

---
+++ tf/2022/otext.tf
@@ -0,0 +1,14 @@
+@config
+@Author=James Tauber
+@Converter=Adrian Negrea
+@Editors=Adrian Negrea, Clacir Virmes, Oliver Glanz, Krysten Thomas
+@Name=MorphGNT:SBLGNT Greek New Testament
+@Note=Feature descriptions adapted from sblgnt README
+@Source:=https://github.com/morphgnt/sblgnt
+@Version=6.12
+@fmt:text-orig-full={text} 
+@sectionFeatures=book,chapter,verse
+@sectionTypes=book,chapter,verse
+@writtenBy=Text-Fabric
+@dateWritten=2021-12-23T03:16:05Z
+
